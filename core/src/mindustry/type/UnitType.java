55098a10314f6f44415bc6fec7f8969a70c351f0
fuzzbuck <54221024+fuzzbuck@users.noreply.github.com>
2020-04-01 00:17:43 +0200

    rest

--- core/src/mindustry/type/UnitType.java
+++ core/src/mindustry/type/UnitType.java
@@ -29,6 +29,7 @@ public class UnitType extends UnlockableContent{
     public float shootCone = 15f;
     public float mass = 1f;
     public boolean flying;
+    public boolean joker = false;
     public boolean targetAir = true;
     public boolean jokerTime = false;
     public boolean rotateWeapon = false;
