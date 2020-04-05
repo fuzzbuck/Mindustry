a1c9628e9ebf98cfde4cca3b56b96915e60d0ce8
fuzzbuck <54221024+fuzzbuck@users.noreply.github.com>
2020-04-05 21:52:18 +0200

    revert random colors :accept:

--- core/src/mindustry/net/NetworkIO.java
+++ core/src/mindustry/net/NetworkIO.java
@@ -64,13 +64,7 @@ public class NetworkIO{
     }
 
     public static ByteBuffer writeServerData(){
-        Random rd = new Random();
-        //String name = (headless ? Config.name.string() : player.name);
-        int ci1 = (int) Mathf.range(0xCCCCCC, 0xffffff); // min & max
-        int ci2 = (int) Mathf.range(0xCCCCCC, 0xffffff); // min & max
-        int ci3 = (int) Mathf.range(0xCCCCCC, 0xffffff); // min & max
-        String c1 = String.format("#%06x", ci1), c2 = String.format("#%06x", ci2), c3 = String.format("#%06x", ci3);
-        String name = "[orange]<[white]io[orange]> [" + c1 + "]mini[" + c2 + "]games [" + c3 + "]\uE84E";
+        String name = (headless ? Config.name.string() : player.name);
         String description = headless && !Config.desc.string().equals("off") ? Config.desc.string() : "";
         String map = world.getMap() == null ? "None" : world.getMap().name();
 
