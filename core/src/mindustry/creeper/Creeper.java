4e8765ebb59fb2eace79ece6a3c6914b741383aa
fuzzbuck <54221024+fuzzbuck@users.noreply.github.com>
2020-12-21 08:40:41 +0100

    stash new creeper behaviour

--- core/src/mindustry/creeper/Creeper.java
+++ core/src/mindustry/creeper/Creeper.java
@@ -22,6 +22,14 @@ public class Creeper {
         tile = world.tile(x, y);
         tile.creeper = this;
     }
+    public Creeper(int _x, int _y, int _level){
+        x = _x;
+        y = _y;
+        level = _level;
+        tile = world.tile(x, y);
+        tile.creeper = this;
+        set(_level);
+    }
     public Creeper(Tile t){
         x = t.x;
         y = t.y;
@@ -29,22 +37,47 @@ public class Creeper {
         tile.creeper = this;
     }
 
+    // whether this creeper is bordering with normal land
+    public boolean isBorderingWithLand(){
+        return (!world.tile(x+1, y).isCreeper()||
+                !world.tile(x-1, y).isCreeper()||
+                !world.tile(x, y+1).isCreeper()||
+                !world.tile(x, y-1).isCreeper()
+                );
+    }
+
+    // whether this creeper is bordering with a creeper
+    public boolean isBorderingWithCreeper(){
+        return (tile.isNearCreeper());
+    }
+
+    // finds free land ready to be claimed (or null if theres no free land)
+    public Tile findAvailableLand(){
+        if(world.tile(x+1, y).isCreeper()) return world.tile(x+1, y);
+        if(world.tile(x-1, y).isCreeper()) return world.tile(x-1, y);
+
+        if(world.tile(x, y+1).isCreeper()) return world.tile(x, y+1);
+        if(world.tile(x, y-1).isCreeper()) return world.tile(x, y-1);
+
+        return null;
+    }
+
     public void set(int _level){
         level = _level;
         world.tile(x, y).setNet(creeperLevelBlocks.get(_level), Team.crux, Mathf.random(0, 3));
         build = world.tile(x, y).build;
     }
-    public void upgrade(){
-        if(level >= max_level)
+    public void upgrade(int amt){
+        if(level+amt >= max_level)
             return;
-        set(level+1);
+        set(level+amt);
     }
-    public void downgrade(){
-        if(level <= 1) {
+    public void downgrade(int amt){
+        if(level-amt <= 1) {
             remove();
             return;
         }
-        set(level-1);
+        set(level-amt);
     }
     public void remove(){
         tile.creeper = null;
--- core/src/mindustry/creeper/CreeperGraph.java
+++ core/src/mindustry/creeper/CreeperGraph.java
@@ -3,16 +3,24 @@ package mindustry.creeper;
 import arc.Events;
 import arc.func.Boolf;
 import arc.func.Cons;
+import arc.math.Mathf;
+import arc.math.geom.Geometry;
+import arc.math.geom.Point2;
 import arc.struct.Seq;
+import mindustry.Vars;
 import mindustry.game.EventType;
+import mindustry.game.Team;
 import mindustry.world.Tile;
+import mindustry.world.blocks.environment.StaticWall;
+
+import java.util.HashMap;
 
 public class CreeperGraph {
-    // all creepers, including "inactive" ones
-    public Seq<Creeper> creepers;
+    // all creepers
+    public HashMap<Point2, Creeper> creepers = new HashMap();
 
-    // creepers that are near a border and are updated very frequently
-    public Seq<Creeper> activeCreepers;
+    // creepers that are near a border to an empty (no creeper) tile
+    public HashMap<Point2, Creeper> activeCreepers = new HashMap();
 
     public CreeperGraph(){
         Events.on(EventType.CreeperDestroyEvent.class, e -> {
@@ -20,37 +28,64 @@ public class CreeperGraph {
                 return;
 
             if(e.tile.creeper.level > 1) {
-                e.tile.creeper.downgrade();
+                e.tile.creeper.downgrade(1);
             }else{
+                Point2 point = new Point2(e.tile.x, e.tile.y);
+                if(creepers.containsValue(e.tile.creeper))
+                    creepers.remove(point);
 
-                if(creepers.contains(e.tile.creeper))
-                    creepers.remove(e.tile.creeper);
-
-                if(activeCreepers.contains(e.tile.creeper))
-                    activeCreepers.remove(e.tile.creeper);
+                if(activeCreepers.containsValue(e.tile.creeper))
+                    activeCreepers.remove(point);
             }
         });
     }
 
-    // upgrades existing creeper
-    public void addOnto(int amount, int chance, int spawnerx, int spawnery){
-        Boolf<Tile> tester = t -> t.isCreeper();
-        Cons<Creeper> setter = c -> {
-            c.upgrade();
-        };
-
-        CreeperUtils.fill(spawnerx, spawnery, tester, setter, amount, chance);
+    public Creeper addCreeper(Point2 point, int amt){
+        if(creepers.containsKey(point)){
+            creepers.get(point).upgrade(amt);
+        }else{
+            creepers.put(point, new Creeper(point.x, point.y, amt));
+        }
+        return creepers.get(point);
     }
 
-    // creates new creeper
-    public void updateBorders(int amount, int chance, int spawnerx, int spawnery){
-        Boolf<Tile> tester = t -> !t.solid() && !t.isCreeper() && t.isNearCreeper();
-        Cons<Creeper> setter = c -> {
-            activeCreepers.add(c);
-            creepers.add(c);
-            c.set(1);
-        };
+    public void putCreeper(int x, int y, int amt, int radius, int chanceSpread){
+        Geometry.circle(x, y, radius, (cx, cy) -> {
+            if(Mathf.random(0, 100) > chanceSpread)
+                return;
 
-        CreeperUtils.fill(spawnerx, spawnery, tester, setter, amount, chance);
+            Tile t = Vars.world.tile(cx, cy);
+            if(t.block() instanceof StaticWall && (t.build != null && !t.isCreeper()))
+                return;
+
+            Point2 point = new Point2(cx, cy);
+            Creeper c = addCreeper(point, amt);
+
+            if(c.isBorderingWithLand()) {
+                activeCreepers.put(point, c);
+            }else{
+                if(activeCreepers.containsKey(point)){
+                    activeCreepers.remove(point);
+                }
+            }
+        });
     }
+
+    public void updateBorders(int amt){
+        for(Creeper c : activeCreepers.values()){
+            if(amt <= 0)
+                return;
+
+            Tile t = c.findAvailableLand();
+            if(t != null){
+                amt--;
+                addCreeper(new Point2(t.x, t.y), c.level == 1 ? 1 : c.level -1);
+            }
+
+            // check if this should still be active
+            if(!c.isBorderingWithLand())
+                activeCreepers.remove(new Point2(c.x, c.y));
+        }
+    }
+
 }
