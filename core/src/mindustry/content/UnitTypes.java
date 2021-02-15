ea0f5eb12533e2cbde6a13ab3b2207b21d9b7838
TranquillyUnpleasant <62061444+TranquillyUnpleasant@users.noreply.github.com>
2021-02-15 14:43:45 +0500

    Gamma

--- core/src/mindustry/content/UnitTypes.java
+++ core/src/mindustry/content/UnitTypes.java
@@ -1422,7 +1422,7 @@ public class UnitTypes implements ContentList{
                     lifetime = 70f;
                     shootEffect = Fx.shootSmall;
                     smokeEffect = Fx.shootSmallSmoke;
-                    tileDamageMultiplier = 2f;
+                    tileDamageMultiplier = 0.5f;
                     homingPower = 0.04f;
                 }};
             }});
